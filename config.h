fbc86073558c371ebae900f82074e30babe97659
Pieter Coremans <pietercoremans@hotmail.com>
2022-02-10 22:53:49 +0100

    commentaren + pcmanfm binding toegevoegd

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -62,21 +62,22 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },/*launch dmenu*/
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },/*launch a terminal*/
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("pcmanfm") },/*launch a pcmanfm*/
+	{ MODKEY,                       XK_b,      togglebar,      {0} },/*toggle top bar*/
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },/*go up a window*/
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },/*go down a window*/
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },/*increment master*/
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },/*decrement master*/
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },/*go left a window*/
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },/*go right a window*/
+	{ MODKEY,                       XK_Return, zoom,           {0} },/*put in master*/
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },/*tiled view*/
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },/*floating view*/
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },/*each window full size*/
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -94,7 +95,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,	        XK_q,      quit,           {0} },/*quit window*/
 };
 
 /* button definitions */
