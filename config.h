dc52266b09b3044f6fc3b2237dcd27c473e6918c
pieter coremans <pieter@pietercoremans.xyz>
2022-08-05 20:13:20 +0200

    first commit on fedora

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 10;       /*gaps size between windows -> m
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=10" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=12" };
 static const char dmenufont[]       = "JetBrainsMono:style=Regular:size=12";
 //background color
 static const char col_gray1[]       = /*"#00374d"*/ "#222222";
@@ -41,7 +41,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* originally 0.55 factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -67,40 +67,48 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "gnome-terminal", NULL };
 
 #include "shiftview.c"
 /*START_KEYS*/
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	// launch programs and scripts: replaced by sxhkd
+	/*{ MODKEY,                       XK_w,      spawn,          SHCMD("firefox") },*//*launch firefox*/
+	/*{ MODKEY,                       XK_e,      spawn,          SHCMD("thunar") },*//*launch a file manager*/
+	/*{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },*//*launch a terminal*/
+	/*{ MODKEY|ShiftMask,             XK_a,      spawn,          SHCMD("$HOME/.local/bin/changekeys") },*//*run changekeys script*/
+	/*{ MODKEY|ShiftMask,             XK_x,      spawn,          SHCMD("$HOME/.local/bin/shutspend") },*//*run shutdown script*/
+	/*{ MODKEY|ShiftMask,             XK_comma,  spawn,          SHCMD("$HOME/.local/bin/dwm_keys") },*//*run dwm keys script*/
+	/*{ MODKEY,                       XK_Up,     spawn,          SHCMD("$HOME/.local/bin/lmc up") },*//*volume up*/
+	/*{ MODKEY,                       XK_Down,   spawn,          SHCMD("$HOME/.local/bin/lmc down") },*//*volume down*/
+	/*{ MODKEY,                       XK_Right,  spawn,          SHCMD("$HOME/.local/bin/pbc up") },*//*screen brightness up*/
+	/*{ MODKEY,                       XK_Left,   spawn,          SHCMD("$HOME/.local/bin/pbc down") },*//*screen brightness down*/
+	/*{ MODKEY|ShiftMask,             XK_w,      spawn,          SHCMD("$HOME/.local/bin/wifi_on_off") },*//*toggle wifi*/
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },/*launch dmenu*/
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },/*launch a terminal*/
-	{ MODKEY,                       XK_e,      spawn,          SHCMD("thunar") },/*launch a file manager*/
-	{ MODKEY,                       XK_w,      spawn,          SHCMD("firefox") },/*launch firefox*/
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },/*toggle top bar*/
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },/*go up a window*/
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },/*go down a window*/
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },/*increment master*/
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },/*decrement master*/
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },/*expand winodw left*/
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },/*expand window left*/
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },/*expand window right*/
 	{ MODKEY,                       XK_Return, zoom,           {0} },/*put in master*/
 	{ MODKEY,                       XK_Tab,    view,           {0} },/*toggle between two last windows*/
 	{ MODKEY,                       XK_q,      killclient,     {0} },/*close window*/
-	{ MODKEY|ShiftMask,             XK_a,      spawn,          SHCMD("$HOME/.local/bin/changekeys") },/*run changekeys script*/
-	{ MODKEY|ShiftMask,             XK_x,      spawn,          SHCMD("$HOME/.local/bin/shutdown_prompt") },/*run shutdown script*/
-	{ MODKEY|ShiftMask,             XK_comma,  spawn,          SHCMD("$HOME/.local/bin/dwm_keys") },/*run dwm keys script*/
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },/*tiled view*/
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },/*floating view*/
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },/*each window full size*/
 	{ MODKEY,                       XK_space,  setlayout,      {0} },/*fix current layout (toggle)*/
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_agrave,      view,           {.ui = ~0 } },/* bekijk alle tags tezamen (maak ongedaan door MODKEY + Control + te verwijderen tag*/
-	{ MODKEY|ShiftMask,             XK_agrave,      tag,            {.ui = ~0 } },/* zet window op alle tags (maak ongedaan door MODKEY|ShiftMask + Control + te verwijderen tag*/
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	/*{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },*/
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+        //bekijk alle tags tezamen (maak ongedaan door MODKEY + Control + te verwijderen tag:
+	{ MODKEY,                       XK_agrave,      view,           {.ui = ~0 } },
+        //zet window op alle tags (maak ongedaan door MODKEY|ShiftMask + Control + te verwijderen tag
+	{ MODKEY|ShiftMask,             XK_agrave,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, /* focus op vorig scherm */
+	{ MODKEY,                       XK_semicolon, focusmon,       {.i = +1 } }, /* focus op volgend scherm */
+	/*{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },*/ /* verplaats window naar vorig scherm*/
+	{ MODKEY|ShiftMask,             XK_semicolon, tagmon,         {.i = +1 } }, /* verplaats window naar volgend scherm*/
 	/*verander gaps tijdens sessie*/
 	{ MODKEY,                       XK_minus, setgaps,         {.i = -1 } },/*maak gaps kleiner*/
 	{ MODKEY,                       XK_equal, setgaps,         {.i = +1 } },/*maak gaps groter*/
@@ -108,11 +116,6 @@ static Key keys[] = {
 	/*Shiftview keys*/
 	{ MODKEY,                       XK_n, shiftview ,          {.i = +1 } },/*ga naar volgende tag*/
 	{ MODKEY,                       XK_b, shiftview,           {.i = -1 } },/*ga naar vorige tag*/ 
-	{ MODKEY,                       XK_Up,    spawn,          SHCMD("$HOME/.local/bin/lmc up") },/*volume up*/
-	{ MODKEY,                       XK_Down,  spawn,          SHCMD("$HOME/.local/bin/lmc down") },/*volume down*/
-	{ MODKEY,                       XK_Right,    spawn,       SHCMD("$HOME/.local/bin/pbc up") },/*screen brightness up*/
-	{ MODKEY,                       XK_Left,  spawn,          SHCMD("$HOME/.local/bin/pbc down") },/*screen brightness down*/
-	{ MODKEY|ShiftMask,             XK_w,     spawn,          SHCMD("$HOME/.local/bin/wifi_on_off") },/*toggle wifi*/
 	/*TAGKEYS(                        XK_1,                      0)*/
 	/*TAGKEYS(                        XK_2,                      1)*/
 	/*TAGKEYS(                        XK_3,                      2)*/
